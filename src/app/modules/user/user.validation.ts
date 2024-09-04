adb78f916ab3d6d061a8578067201fb7e77c8db4
tonoy3125 <shaifshajedt@gmail.com>
2024-09-04 18:08:26 +0600

    user validation added

---
+++ src/app/modules/user/user.validation.ts
@@ -0,0 +1,18 @@
+import { z } from 'zod'
+import { USER_ROLE } from './user.constant'
+
+const createUserValidationSchema = z.object({
+  body: z.object({
+    firstName: z.string({ required_error: 'First Name is required' }),
+    lastName: z.string({ required_error: 'Last Name is required' }),
+    email: z
+      .string({ required_error: 'Email is required' })
+      .email({ message: 'Invalid email' }),
+    password: z.string({ required_error: 'Password is required' }),
+    role: z.nativeEnum(USER_ROLE),
+  }),
+})
+
+export const UserValidations = {
+  createUserValidationSchema,
+}

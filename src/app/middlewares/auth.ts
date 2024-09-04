85994b82f1bf09274ea3fd51eb99e08cdd8bdd34
tonoy3125 <shaifshajedt@gmail.com>
2024-09-04 21:30:45 +0600

    auth middleware added commit

---
+++ src/app/middlewares/auth.ts
@@ -0,0 +1,50 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import jwt, { JwtPayload } from 'jsonwebtoken'
+import { NextFunction, Request, Response } from 'express'
+import { TUserRoles } from '../modules/user/user.interface'
+import config from '../config'
+import { User } from '../modules/user/user.model'
+import httpStatus from 'http-status'
+
+const auth = (...requiredRoles: TUserRoles[]) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const token = req.headers.authorization?.split(' ')[1]
+
+      if (!token) {
+        throw new Error()
+      }
+
+      const decoded = jwt.verify(
+        token as string,
+        config.jwt_access_secret as string,
+      ) as JwtPayload
+      const { email, role } = decoded
+
+      const user = User.findOne({ email, role })
+
+      if (!user) {
+        throw new Error()
+      }
+      // console.log(user)
+
+      if (!requiredRoles.includes(role)) {
+        throw new Error()
+      }
+      req.user = {
+        email,
+        role,
+      }
+
+      next()
+    } catch (err) {
+      res.status(404).json({
+        success: false,
+        statusCode: httpStatus.UNAUTHORIZED,
+        message: 'You have no access to this route',
+      })
+    }
+  }
+}
+
+export default auth
--- src/app/modules/auth/auth.route.ts
+++ src/app/modules/auth/auth.route.ts
@@ -10,4 +10,6 @@ router.post(
   AuthControllers.signUp,
 )
 
+
+
 export const AuthRoutes = router
